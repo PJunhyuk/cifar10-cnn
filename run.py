84efd2052f57c92e69d0c859b9eeeed057932e16
MCML-PJunhyuk-LabDT <jgravity@naver.com>
2018-05-25 17:50:26 +0900

    Add other networks

--- run.py
+++ run.py
@@ -33,14 +33,40 @@ if __name__ == "__main__":
             self.pool = nn.MaxPool2d(2, 2)
             self.fc = nn.Linear(1152, 10)
 
+#            self.conv = nn.Conv2d(3, 180, 5)
+#            self.pool = nn.MaxPool2d(2, 2)
+#            self.fc = nn.Linear(14*14*180, 10)
+
+            #self.conv1 = nn.Conv2d(3, 64, 5) # 32 -> 28 -> 14
+            #self.conv2 = nn.Conv2d(64, 64, 5) # 14 -> 10 -> 5
+            #self.conv3 = nn.Conv2d(64, 128, 3) # 5 -> 3
+            #self.conv4 = nn.Conv2d(128, 128, 3) # 3 -> 1
+            #self.pool = nn.MaxPool2d(2, 2)
+            #self.fc = nn.Linear(1*1*128, 10)
+
         def forward(self, x):
             x = F.relu(self.conv1(x)) ## 32x32x3 -> 28x28x64
             x = self.pool(x) ## 28x28x64 -> 14x14x64
             x = F.relu(self.conv2(x)) ## 14x14x64 -> 10x10x128
             x = self.pool(x) ## 10x10x128 -> 5x5x128
             x = F.relu(self.conv3(x)) ## 5x5x128 -> 3x3x128
-            x = x.view(-1, 3 * 3 * 128) ## 3x3x128 -> 1152
+            x = x.view(-1, 3*3*128) ## 3x3x128 -> 1152
             x = self.fc(x) ## 1152 -> 10
+
+            # x = F.relu(self.conv(x))
+            # x = self.pool(x)
+            # x = x.view(-1, 14*14*180)
+            # x = self.fc(x)
+
+            #x = F.relu(self.conv1(x))
+            #x = self.pool(x)
+            #x = F.relu(self.conv2(x))
+            #x = self.pool(x)
+            #x = F.relu(self.conv3(x))
+            #x = F.relu(self.conv4(x))
+            #x = x.view(-1, 1*1*128)
+            #x = self.fc(x)
+
             return x
 
     net = Net()
@@ -70,29 +96,30 @@ if __name__ == "__main__":
         #### Checking train loss
         time_start_loop = time.time()
         for i, data in enumerate(trainloader, 0):
-            inputs, labels = data
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss=criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            train_num = train_num + 1
+            if i % 50 == 0:
+                inputs, labels = data
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                loss=criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+                train_num = train_num + 1
         train_loss_list.append(running_loss)
         print('iteration: %d ==> loss: %.3f' % (epoch + 1, running_loss / train_num))
         print('Required time(s): ' + str(time.time() - time_start_loop))
 
         #### Checking test loss
-        time_start_loop = time.time()
-        for i, data in enumerate(testloader, 0):
-            inputs, labels = data
-            outputs = net(inputs)
-            loss=criterion(outputs, labels)
-            test_loss += loss.item()
-            test_num = test_num + 1
-        test_loss_list.append(test_loss)
-        print('iteration: %d ==> test_loss: %.3f' % (epoch + 1, test_loss / test_num))
-        print('Required time(s): ' + str(time.time() - time_start_loop))
+#        time_start_loop = time.time()
+#        for i, data in enumerate(testloader, 0):
+#            inputs, labels = data
+#            outputs = net(inputs)
+#            loss=criterion(outputs, labels)
+#            test_loss += loss.item()
+#            test_num = test_num + 1
+#        test_loss_list.append(test_loss)
+#        print('iteration: %d ==> test_loss: %.3f' % (epoch + 1, test_loss / test_num))
+#        print('Required time(s): ' + str(time.time() - time_start_loop))
 
         ## Checking accuracy
         time_start_test = time.time()
