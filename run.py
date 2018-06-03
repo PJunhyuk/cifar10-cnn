7f0ee75b993666a0b41848387f68e41f0934c00f
MCML-PJunhyuk-LabDT <jgravity@naver.com>
2018-06-03 23:48:38 +0900

    Update network

--- run.py
+++ run.py
@@ -27,11 +27,14 @@ if __name__ == "__main__":
     class Net(nn.Module):
         def __init__(self):
             super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(3, 64, 5)
-            self.conv2 = nn.Conv2d(64, 128, 5)
-            self.conv3 = nn.Conv2d(128, 128, 3)
+            self.conv1 = nn.Conv2d(3, 64, 5, padding=2) # 32 -> 32
+            self.conv2 = nn.Conv2d(64, 64, 5, padding=2) # 16 -> 16
+            self.conv3 = nn.Conv2d(64, 128, 3, padding=1) # 8 -> 8
+            self.conv4 = nn.Conv2d(128, 128, 3, padding=1) # 8 -> 8
+            self.conv5 = nn.Conv2d(128, 128, 3, padding=1) # 8 -> 8
             self.pool = nn.MaxPool2d(2, 2)
-            self.fc = nn.Linear(1152, 10)
+            self.fc1 = nn.Linear(8192, 384)
+            self.fc2 = nn.Linear(384, 10)
 
             # self.conv = nn.Conv2d(3, 180, 5)
             # self.pool = nn.MaxPool2d(2, 2)
@@ -45,13 +48,17 @@ if __name__ == "__main__":
             # self.fc = nn.Linear(1*1*128, 10)
 
         def forward(self, x):
-            x = F.relu(self.conv1(x)) ## 32x32x3 -> 28x28x64
-            x = self.pool(x) ## 28x28x64 -> 14x14x64
-            x = F.relu(self.conv2(x)) ## 14x14x64 -> 10x10x128
-            x = self.pool(x) ## 10x10x128 -> 5x5x128
-            x = F.relu(self.conv3(x)) ## 5x5x128 -> 3x3x128
-            x = x.view(-1, 3*3*128) ## 3x3x128 -> 1152
-            x = self.fc(x) ## 1152 -> 10
+            x = F.relu(self.conv1(x)) ## 32x32x3 -> 32x32x64
+            x = self.pool(x) ## 32x32x64 -> 16x16x64
+            x = F.relu(self.conv2(x)) ## 16x16x64 -> 16x16x64
+            x = self.pool(x) ## 16x16x64 -> 8x8x64
+            x = F.relu(self.conv3(x)) ## 8x8x64 -> 8x8x128
+            x = F.relu(self.conv4(x)) ## 8x8x128 -> 8x8x128
+            x = F.relu(self.conv5(x)) ## 8x8x128 -> 8x8x128
+            x = x.view(-1, 8*8*128) ## 8x8x128 -> 8192
+            x = F.relu(self.fc1(x)) ## 8192 -> 384
+            x = F.dropout(x, training=self.training)
+            x = self.fc2(x) ## 384 -> 10
 
             # x = F.relu(self.conv(x))
             # x = self.pool(x)
@@ -110,16 +117,16 @@ if __name__ == "__main__":
         print('Required time(s): ' + str(time.time() - time_start_loop))
 
         #### Checking test loss
-       time_start_loop = time.time()
-       for i, data in enumerate(testloader, 0):
-           inputs, labels = data
-           outputs = net(inputs)
-           loss=criterion(outputs, labels)
-           test_loss += loss.item()
-           test_num = test_num + 1
-       test_loss_list.append(test_loss)
-       print('iteration: %d ==> test_loss: %.3f' % (epoch + 1, test_loss / test_num))
-       print('Required time(s): ' + str(time.time() - time_start_loop))
+        time_start_loop = time.time()
+        for i, data in enumerate(testloader, 0):
+            inputs, labels = data
+            outputs = net(inputs)
+            loss=criterion(outputs, labels)
+            test_loss += loss.item()
+            test_num = test_num + 1
+        test_loss_list.append(test_loss)
+        print('iteration: %d ==> test_loss: %.3f' % (epoch + 1, test_loss / test_num))
+        print('Required time(s): ' + str(time.time() - time_start_loop))
 
         ## Checking accuracy
         time_start_test = time.time()
