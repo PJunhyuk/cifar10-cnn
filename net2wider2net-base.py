769ff8aa643056d27988ebdfc2166911510090c0
MCML-PJunhyuk-LabDT <jgravity@naver.com>
2018-12-18 11:30:08 +0900

    Add net2wider2net

--- net2wider2net-base.py
+++ net2wider2net-base.py
@@ -51,17 +51,19 @@ if __name__ == "__main__":
 
             return x
 
-    class Net2Deeper1Net(nn.Module):
+    class Net2Wider2Net(nn.Module):
         def __init__(self):
-            super(Net2Deeper1Net, self).__init__()
-            self.conv1 = nn.Conv2d(3, 64, 5, padding=2) # 32 -> 32
-            self.conv2 = nn.Conv2d(64, 64, 5, padding=2) # 16 -> 16
-            self.conv3 = nn.Conv2d(64, 128, 3, padding=1) # 8 -> 8
-            self.conv4 = nn.Conv2d(128, 128, 3, padding=1) # 8 -> 8
-            self.conv5 = nn.Conv2d(128, 128, 3, padding=1) # 8 -> 8
-            self.conv6 = nn.Conv2d(128, 128, 3, padding=1) # 8 -> 8
+            # 64 * 1.2 = 77
+            # 128 * 1.2 = 154
+            # 154 * 8 * 8 = 9856
+            super(Net2Wider2Net, self).__init__()
+            self.conv1 = nn.Conv2d(3, 77, 5, padding=2) # 32 -> 32
+            self.conv2 = nn.Conv2d(77, 77, 5, padding=2) # 16 -> 16
+            self.conv3 = nn.Conv2d(77, 154, 3, padding=1) # 8 -> 8
+            self.conv4 = nn.Conv2d(154, 154, 3, padding=1) # 8 -> 8
+            self.conv5 = nn.Conv2d(154, 154, 3, padding=1) # 8 -> 8
             self.pool = nn.MaxPool2d(2, 2)
-            self.fc1 = nn.Linear(8192, 384)
+            self.fc1 = nn.Linear(9856, 384)
             self.fc2 = nn.Linear(384, 10)
 
         def forward(self, x):
@@ -72,26 +74,39 @@ if __name__ == "__main__":
             x = F.relu(self.conv3(x)) ## 8x8x64 -> 8x8x128
             x = F.relu(self.conv4(x)) ## 8x8x128 -> 8x8x128
             x = F.relu(self.conv5(x)) ## 8x8x128 -> 8x8x128
-            x = F.relu(self.conv6(x)) ## 8x8x128 -> 8x8x128
-            x = x.view(-1, 8*8*128) ## 8x8x128 -> 8192
+            x = x.view(-1, 8*8*154) ## 8x8x128 -> 8192
             x = F.relu(self.fc1(x)) ## 8192 -> 384
             x = F.dropout(x, training=self.training)
             x = self.fc2(x) ## 384 -> 10
 
             return x
 
-    net = Net2Deeper1Net()
+    net = Net2Wider2Net()
     net_teacher = Net()
 
     #######################################################################################
 
     net_teacher.load_state_dict(torch.load('./checkpoint/checkpoint_teacher_399.pth'))
 
-    net._modules['conv1'].weight.data = net_teacher._modules['conv1'].weight.data
-    net._modules['conv2'].weight.data = net_teacher._modules['conv2'].weight.data
-    net._modules['conv3'].weight.data = net_teacher._modules['conv3'].weight.data
-    net._modules['conv4'].weight.data = net_teacher._modules['conv4'].weight.data
-    net._modules['conv5'].weight.data = net_teacher._modules['conv5'].weight.data
+    for i in range(0, 64):
+        for j in range(0, 3):
+            net._modules['conv1'].weight.data[i][j] = net_teacher._modules['conv1'].weight.data[i][j]
+
+    for i in range(0, 64):
+        for j in range(0, 64):
+            net._modules['conv2'].weight.data[i][j] = net_teacher._modules['conv2'].weight.data[i][j]
+
+    for i in range(0, 128):
+        for j in range(0, 64):
+            net._modules['conv3'].weight.data[i][j] = net_teacher._modules['conv3'].weight.data[i][j]
+
+    for i in range(0, 128):
+        for j in range(0, 128):
+            net._modules['conv4'].weight.data[i][j] = net_teacher._modules['conv4'].weight.data[i][j]
+
+    for i in range(0, 128):
+        for j in range(0, 128):
+            net._modules['conv5'].weight.data[i][j] = net_teacher._modules['conv5'].weight.data[i][j]
 
     #######################################################################################
     ## 3. Define a Loss function and optimizer≠
@@ -162,7 +177,7 @@ if __name__ == "__main__":
         print('Required time(s): ' + str(time.time() - time_start_test))
 
         time_start_save = time.time()
-        filename = './checkpoint/checkpoint_deeper1_base_' + str(epoch) + '.pth'
+        filename = './checkpoint/checkpoint_wider2_base_' + str(epoch) + '.pth'
         torch.save(net.state_dict(), filename)
         print('save complete!')
 
